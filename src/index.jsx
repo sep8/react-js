256f750acd9d81b7299843a9828fa40e852f23af
Mingyang Han <mingyang_han@esri.com>
2021-09-14 13:49:23 +0800

    chore: init

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const HelloWorld = () => {
+    const [Another, setAnother] = React.useState('div')
+
+    React.useEffect(() => {
+        import('./another').then((module) => {
+            setAnother(module.Another)
+        })
+    }, [])
+
     return (
-        <h1>
-            Hello World
-        </h1>
+        <>
+            <h1>
+                Hello World
+            </h1>
+            {Another}
+        </>
     );
 }
 
